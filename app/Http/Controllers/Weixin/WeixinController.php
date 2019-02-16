be2a9ade5380ece8c91ac0e56dc3a941c85d7380
dante <dantinr@gmail.com>
2019-02-16 13:56:06 +0800

    weixin test

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -20,6 +20,17 @@ class WeixinController extends Controller
         $this->getUserInfo(1);
     }
 
+    /**
+     * 接收事件推送
+     */
+    public function validToken1()
+    {
+        //$get = json_encode($_GET);
+        //$str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
+        //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
+        echo $_GET['echostr'];
+    }
+
 
     /**
      * 接收事件推送
