8ef78d628aaa7e64bde003cd4ea2dbf6ab85d4bd
dante <dantinr@gmail.com>
2019-02-23 09:33:49 +0800

    debug

--- resources/views/test/form.blade.php
+++ resources/views/test/form.blade.php
@@ -1,14 +1,3 @@
-<!doctype html>
-<html lang="zh">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel-Blade</title>
-</head>
-<body>
-    <h1>表单测试</h1>
 
 
     <form action="/form/test" method="post" enctype="multipart/form-data">
@@ -19,5 +8,3 @@
 
         <input type="submit" value="SUBMIT">
     </form>
-</body>
-</html>
