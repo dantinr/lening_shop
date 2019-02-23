4a48b640fc994a8ce4bb4aa553e6f2c0e1a7f923
dante <dantinr@gmail.com>
2019-02-23 13:11:42 +0800

    客服聊天

--- routes/web.php
+++ routes/web.php
@@ -175,7 +175,11 @@ Route::get('/test/405','Weixin\WeixinController@http405');     //获取永久素
 Route::get('/weixin/material/list','Weixin\WeixinController@materialList');     //获取永久素材列表
 Route::get('/weixin/material/upload','Weixin\WeixinController@upMaterial');     //上传永久素材
 Route::post('/weixin/material','Weixin\WeixinController@materialTest');     //创建菜单
-//Route::post('/weixin/material','Weixin\WeixinController@materialTest');     //创建菜单
+
+//微信聊天
+Route::get('/weixin/kefu/chat','Weixin\WeixinController@chatView');     //客服聊天
+Route::get('/weixin/chat/get_msg','Weixin\WeixinController@getChatMsg');     //获取用户聊天信息
+
 
 
 
