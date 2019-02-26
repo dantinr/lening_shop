d04a0320ab7f2a22577d0d322ef6bbdd7fb7e34e
dante <dantinr@gmail.com>
2019-02-26 11:44:14 +0800

    debug

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -17,23 +17,24 @@ class PayController extends Controller
     public function test()
     {
 
-        echo __METHOD__;echo '<hr>';
 
-        $total_fee = 1;
+        //
+        $total_fee = 1;         //用户要支付的总金额
+        $order_id = OrderModel::generateOrderSN();
 
         $order_info = [
-            'appid'         =>  env('WEIXIN_APPID_0'),
-            'mch_id'        =>  env('WEIXIN_MCH_ID'),
-            'nonce_str'     => str_random(16),
-            //'sign'          => '',
+            'appid'         =>  env('WEIXIN_APPID_0'),      //微信支付绑定的服务号的APPID
+            'mch_id'        =>  env('WEIXIN_MCH_ID'),       // 商户ID
+            'nonce_str'     => str_random(16),             // 随机字符串
             'sign_type'     => 'MD5',
             'body'          => '测试订单-'.mt_rand(1111,9999) . str_random(6),
-            'out_trade_no'  => OrderModel::generateOrderSN(),
+            'out_trade_no'  => $order_id,                       //本地订单号
             'total_fee'     => $total_fee,
             'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],     //客户端IP
-            'notify_url'    => $this->weixin_notify_url,
-            'trade_type'    => 'NATIVE'
+            'notify_url'    => $this->weixin_notify_url,        //通知回调地址
+            'trade_type'    => 'NATIVE'                         // 交易类型
         ];
+
 
         $this->values = [];
         $this->values = $order_info;
@@ -43,20 +44,21 @@ class PayController extends Controller
         $rs = $this->postXmlCurl($xml, $this->weixin_unifiedorder_url, $useCert = false, $second = 30);
 
         $data =  simplexml_load_string($rs);
-        var_dump($data);echo '<hr>';
-        echo 'return_code: '.$data->return_code;echo '<br>';
-		echo 'return_msg: '.$data->return_msg;echo '<br>';
-		echo 'appid: '.$data->appid;echo '<br>';
-		echo 'mch_id: '.$data->mch_id;echo '<br>';
-		echo 'nonce_str: '.$data->nonce_str;echo '<br>';
-		echo 'sign: '.$data->sign;echo '<br>';
-		echo 'result_code: '.$data->result_code;echo '<br>';
-		echo 'prepay_id: '.$data->prepay_id;echo '<br>';
-		echo 'trade_type: '.$data->trade_type;echo '<br>';
+//        //var_dump($data);echo '<hr>';
+//        echo 'return_code: '.$data->return_code;echo '<br>';
+//		echo 'return_msg: '.$data->return_msg;echo '<br>';
+//		echo 'appid: '.$data->appid;echo '<br>';
+//		echo 'mch_id: '.$data->mch_id;echo '<br>';
+//		echo 'nonce_str: '.$data->nonce_str;echo '<br>';
+//		echo 'sign: '.$data->sign;echo '<br>';
+//		echo 'result_code: '.$data->result_code;echo '<br>';
+//		echo 'prepay_id: '.$data->prepay_id;echo '<br>';
+//		echo 'trade_type: '.$data->trade_type;echo '<br>';
 		echo 'code_url: '.$data->code_url;echo '<br>';
-        die;
+//        die;
         //echo '<pre>';print_r($data);echo '</pre>';
 
+        //将 code_url 返回给前端，前端生成 支付二维码
 
     }
 
@@ -168,14 +170,26 @@ class PayController extends Controller
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents('logs/wx_pay_notice.log',$log_str,FILE_APPEND);
 
+        $xml = simplexml_load_string($data);
 
-        //逻辑处理
-        if(1){      //成功
-            $response = '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
-            echo $response;
-        }else{
-            //TODO 处理异常情况
+        if($xml->result_code=='SUCCESS' && $xml->return_code=='SUCCESS'){      //微信支付成功回调
+            //验证签名
+            $sign = true;
+
+            if($sign){       //签名验证成功
+                //TODO 逻辑处理  订单状态更新
+
+            }else{
+                //TODO 验签失败
+                echo '验签失败，IP: '.$_SERVER['REMOTE_ADDR'];
+                // TODO 记录日志
+            }
+
         }
+
+        $response = '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
+        echo $response;
+
     }
 
 
