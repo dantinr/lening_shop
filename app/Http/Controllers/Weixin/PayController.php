8d46b00e540d118d72d8491d1e8fdfb6e6be0a53
dante <dantinr@gmail.com>
2019-02-26 11:05:23 +0800

    微信支付回调处理

---
+++ app/Http/Controllers/Weixin/PayController.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Http\Controllers\Weixin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Weixin\WXBizDataCryptController;
+use App\Model\OrderModel;
+
+class PayController extends Controller
+{
+    //
+
+    public $weixin_unifiedorder_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
+    public $weixin_notify_url = 'http://shop.comcto.com/weixin/pay/notice';     //支付通知回调
+
+    public function test()
+    {
+
+        echo __METHOD__;echo '<hr>';
+
+        $total_fee = 1;
+
+        $order_info = [
+            'appid'         =>  env('WEIXIN_APPID_0'),
+            'mch_id'        =>  env('WEIXIN_MCH_ID'),
+            'nonce_str'     => str_random(16),
+            //'sign'          => '',
+            'sign_type'     => 'MD5',
+            'body'          => '测试订单-'.mt_rand(1111,9999) . str_random(6),
+            'out_trade_no'  => OrderModel::generateOrderSN(),
+            'total_fee'     => $total_fee,
+            'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],     //客户端IP
+            'notify_url'    => $this->weixin_notify_url,
+            'trade_type'    => 'NATIVE'
+        ];
+
+        $this->values = [];
+        $this->values = $order_info;
+        $this->SetSign();
+
+        $xml = $this->ToXml();      //将数组转换为XML
+        $rs = $this->postXmlCurl($xml, $this->weixin_unifiedorder_url, $useCert = false, $second = 30);
+
+        $data =  simplexml_load_string($rs);
+        var_dump($data);echo '<hr>';
+        echo 'return_code: '.$data->return_code;echo '<br>';
+		echo 'return_msg: '.$data->return_msg;echo '<br>';
+		echo 'appid: '.$data->appid;echo '<br>';
+		echo 'mch_id: '.$data->mch_id;echo '<br>';
+		echo 'nonce_str: '.$data->nonce_str;echo '<br>';
+		echo 'sign: '.$data->sign;echo '<br>';
+		echo 'result_code: '.$data->result_code;echo '<br>';
+		echo 'prepay_id: '.$data->prepay_id;echo '<br>';
+		echo 'trade_type: '.$data->trade_type;echo '<br>';
+		echo 'code_url: '.$data->code_url;echo '<br>';
+        die;
+        //echo '<pre>';print_r($data);echo '</pre>';
+
+
+    }
+
+
+    protected function ToXml()
+    {
+        if(!is_array($this->values)
+            || count($this->values) <= 0)
+        {
+            die("数组数据异常！");
+        }
+        $xml = "<xml>";
+        foreach ($this->values as $key=>$val)
+        {
+            if (is_numeric($val)){
+                $xml.="<".$key.">".$val."</".$key.">";
+            }else{
+                $xml.="<".$key."><![CDATA[".$val."]]></".$key.">";
+            }
+        }
+        $xml.="</xml>";
+        return $xml;
+    }
+
+    private  function postXmlCurl($xml, $url, $useCert = false, $second = 30)
+    {
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//		if($useCert == true){
+//			//设置证书
+//			//使用证书：cert 与 key 分别属于两个.pem文件
+//			curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+//			curl_setopt($ch,CURLOPT_SSLCERT, WxPayConfig::SSLCERT_PATH);
+//			curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+//			curl_setopt($ch,CURLOPT_SSLKEY, WxPayConfig::SSLKEY_PATH);
+//		}
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if($data){
+            curl_close($ch);
+            return $data;
+        } else {
+            $error = curl_errno($ch);
+            curl_close($ch);
+            die("curl出错，错误码:$error");
+        }
+    }
+
+
+    public function SetSign()
+    {
+        $sign = $this->MakeSign();
+        $this->values['sign'] = $sign;
+        return $sign;
+    }
+
+
+    private function MakeSign()
+    {
+        //签名步骤一：按字典序排序参数
+        ksort($this->values);
+        $string = $this->ToUrlParams();
+        //签名步骤二：在string后加入KEY
+        $string = $string . "&key=".env('WEIXIN_MCH_KEY');
+        //签名步骤三：MD5加密
+        $string = md5($string);
+        //签名步骤四：所有字符转为大写
+        $result = strtoupper($string);
+        return $result;
+    }
+
+    /**
+     * 格式化参数格式化成url参数
+     */
+    protected function ToUrlParams()
+    {
+        $buff = "";
+        foreach ($this->values as $k => $v)
+        {
+            if($k != "sign" && $v != "" && !is_array($v)){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+
+    /**
+     * 微信支付回调
+     */
+    public function notice()
+    {
+        $data = file_get_contents("php://input");
+
+        //记录日志
+        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        file_put_contents('logs/wx_pay_notice.log',$log_str,FILE_APPEND);
+
+
+        //逻辑处理
+        if(1){      //成功
+            $response = '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
+            echo $response;
+        }else{
+            //TODO 处理异常情况
+        }
+    }
+
+
+}
