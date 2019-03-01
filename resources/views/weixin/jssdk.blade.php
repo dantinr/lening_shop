ae2192d75bb782bb80b0d4f7af5668f9e5d2680c
dante <dantinr@gmail.com>
2019-03-01 11:53:18 +0800

    相册 接口调试

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         <h2>JSSDK</h2>
 
+        <button id="btn1">选择照片</button>
+
     </div>
 @endsection
 @section('footer')
@@ -23,14 +25,18 @@
 
         wx.ready(function(){
 
-            wx.chooseImage({
-                count: 9, // 默认9
-                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                success: function (res) {
-                    var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                }
+
+            $("#btn1").click(function () {
+                wx.chooseImage({
+                    count: 9, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                    success: function (res) {
+                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    }
+                });
             });
+            
         });
     </script>
 @endsection
