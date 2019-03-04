f3780f9494173523c206845d68aaea24cfeb20f3
dante <dantinr@gmail.com>
2019-03-04 16:06:19 +0800

    导航 + 聊天室

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,6 +74,7 @@
                         <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3a%2f%2fmall.77sc.com.cn%2fweixin.php%3fr1%3dhttp%3a%2f%2fshop.comcto.com%2fweixin%2fgetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect"><img src="/imgs/wx_logo_24.png" alt=""></a>
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
+                        <a href="http://chat.comcto.com" target="_blank">聊天室</a>
                     @endauth
                 </div>
             @endif
